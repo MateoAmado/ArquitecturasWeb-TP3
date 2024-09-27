79b9f436f507af84aa1b2d54f3e0962d61e62727
Gonzalohellers <86278786+Gonzalohellers@users.noreply.github.com>
2024-09-27 17:57:11 -0300

    starter web añadido al pom

--- src/main/java/org/integrador3/IntegradorTp3Application.java
+++ src/main/java/org/integrador3/IntegradorTp3Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntegradorTp3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorTp3Application.class, args);
 	}
-
 }
