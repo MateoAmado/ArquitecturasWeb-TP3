3696b6935157a8212811bb4fd823094fb01c549e
Mateo Amado <elmate0014@gmail.com>
2024-09-27 15:59:07 -0300

    Initial commmit

---
+++ src/main/java/org/integrador3/IntegradorTp3Application.java
@@ -0,0 +1,13 @@
+package org.integrador3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorTp3Application.class, args);
+	}
+
+}
